8ca40be64340d93acffd260aeba070ee5fef9dc6
william dutra <wil.liam_benicio@hotmail.com>
2023-11-15 11:56:30 -0300

    swagger na aplicação

--- cadastro/src/main/java/com/UsuariosCadastro/cadastro/CadastroApplication.java
+++ cadastro/src/main/java/com/UsuariosCadastro/cadastro/CadastroApplication.java
@@ -3,7 +3,10 @@ package com.UsuariosCadastro.cadastro;
 import jdk.jfr.Enabled;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableAsync;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableFeignClients
